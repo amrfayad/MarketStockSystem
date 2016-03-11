0ec9742bcdf4dea64b809939f00923054f4dd869
ManarAhmed <eng.mema2014@yahoo.com.au>
2016-03-11 22:19:54 +0200

    add alarm with ajax and ask the user before delete

---
+++ resources/js/projectFiles/addAlert.js
@@ -0,0 +1,49 @@
+(function ($){
+    
+    var sh_id,user_id,direction,price,last_trig,sh_price,txt,trig_txt;
+    $("#ok").on("click",function (){
+        sh_id = $("#share option:selected").attr("id");
+        user_id = $("#user_id").val();
+        direction = $("#optradio").val();
+        price = $("#price").val();
+        last_trig = $("#alarm_triggered").val();
+        sh_price = $("#share option:selected").val();
+        if (direction == 0) {
+            txt ="Drops Below " + price;
+        } else {
+            txt ="Goes Up " + price;
+        }
+        if (!last_trig){
+            trig_txt= "Never";
+        } else {
+            trig_txt= last_trig+"well";
+        }
+        var setting = {
+                type:"POST",
+                data: {"user_id":user_id,"sh_id":sh_id,"direction":direction, "price":price} , 
+                url:"http://localhost/php-project/MarketStockSystem/controller/addAlarm.php",
+                success:function(response){
+                    alert(response);
+                    $("#alertTable").append(
+'<tr>'+
+    '<input type="hidden" id="alarm_id" name="alarm_id" value="1">'+
+    '<td scope="row">'+
+        '<div class="checkbox">'+       
+            '<label><input type="checkbox" class="checkbx" value="" checked></label>'+
+        '</div>'+
+   '</td>'+
+    '<td>'+$("#share option:selected").text()+'</td>'+
+    '<td>'+sh_price+'</td>'+
+    '<td>'+txt+'</td>'+
+    '<td> '+trig_txt+'</td>'+
+    '<td><button class="btn btn-link delete" role="link" type="submit"  name="delete" value="Link 2">delete</button>\n\
+    </td>'+
+
+'</tr>')
+                    
+                        }  
+                    };
+             $.ajax(setting);
+    });
+})(jQuery);
+
