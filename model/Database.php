9d02d45ee241138955059292f86c8dd764e9da16
EngHamda <eng.nhamda@yahoo.com>
2016-03-11 15:26:55 +0200

    registration without using ajax

--- model/Database.php
+++ model/Database.php
@@ -3,7 +3,7 @@ class Database{
 	
  static function connect()
   {
-    return mysqli_connect("localhost","root","root","stock_market");
+    return mysqli_connect("localhost","root","ROOT","stock_market");
     
   }
  
